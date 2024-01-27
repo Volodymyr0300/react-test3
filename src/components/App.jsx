a1a56bb4629c81daa8000e4d6d505727a856b34b
Volodymyr Poliakov <vladimir03002@gmail.com>
2024-01-26 22:57:37 -0600

    just practice in react operators and props

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+// src/components/App.jsx
+
+// import { Product } from "./Product";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product name="Tacos With Lime" price={10.99} />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+
+// import UserGreeting from "./Product.jsx";
+
+// function App() {
+//   return (
+//     <>
+//       <UserGreeting isLoggedIn={false} username="BroCode" />
+//     </>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,26 @@
+// src/components/Product.jsx
+
+// export const Product = ({
+//   name,
+//   imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+//   price,
+// }) => (
+//   <div>
+//     <h2>{name}</h2>
+//     <img src={imgUrl} alt={name} width="480" />
+//     <p>Price: {price} credits</p>
+//   </div>
+// );
+
+// function UserGreeting(props) {
+//   const welcomeMessage = (
+//     <h2 className="welcome-message">Welcome {props.username}</h2>
+//   );
+//   const loginMessage = (
+//     <h2 className="login-promt">Please log in to continue</h2>
+//   );
+
+//   return props.isLoggedIn ? welcomeMessage : loginMessage;
+// }
+
+// export default UserGreeting;
